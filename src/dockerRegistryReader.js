1446a6dae73289d9c3d4fcbd7331ea525ff2fa5f
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-08-29 08:06:34 -0400

    Start-of-client (#3)
    * Create copy of JSDoc's default template
    
    * Create registry client
    
    * Add tests
    
    * Remove header
    
    * Add docker hub registry wrapper
    
    * Add test and jsdocs scripts
    
    * Add renovate.json (#1)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    (cherry picked from commit 269315c263445968d266e4c10e510e319bb4ba29)
    
    * Create node.js.yml
    
    (cherry picked from commit 12f880c448990c83af03a4ffb2f8004a30163d09)
    
    * Update node.js.yml
    
    * Create codeql.yml
    
    (cherry picked from commit 1ebd1ce639ff4533ba98c57172902a6f31a28b6e)
    
    * Correct copyright year

--- src/dockerRegistryReader.js
+++ src/dockerRegistryReader.js
@@ -1,25 +1,31 @@
 // Buoy is a image layer scanner
-// Copyright (C) 2000  Drazi Crendraven
-
+// Copyright (C) 2022  Drazi Crendraven
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-export const DOCKER_AUTH_BASE = "https://auth.docker.io/token?service=registry.docker.io&scope="
+import { ContainerRegistryClient } from "../lib/containerRegistryClient.js";
 
-export const MANIFEST_ENDPOINT = "/manifests/"
+export class DockerRegistryClient extends ContainerRegistryClient {
+    /**
+     * 
+     * @param {string} imageName 
+     */
+    constructor(imageName) {
+        super("registry.docker.com")
+        this.setImageName(imageName)
+        this.setAuthOptions({ authHost: "auth.docker.io", authService: "registry.docker.io"})
+    }
 
-export const TAGS_ENDPOINT = "/tags/list"
-
-export const BLOB_ENDPOINT = "/blobs/"
-
-export const DOCKER_API_BASE = "https://registry.docker.com/v2/"
+    
+}
